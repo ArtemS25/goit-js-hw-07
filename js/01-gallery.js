1b67e530d8f330b79339a26bb335fca1299744b0
ArtemS25 <sribnenko@gmail.com>
2022-12-16 15:20:53 +0200

    Finished task 1-2

---
+++ js/01-gallery.js
@@ -0,0 +1,38 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image"
+      src="${preview}"
+      data-source="large-image.jpg"
+      alt="${description}"/>
+    </a>
+    </div>`
+  )
+  .join("");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+galleryEl.addEventListener("click", onImgGalleryClick);
+
+let instance = "";
+
+function onImgGalleryClick(event) {
+  event.preventDefault();
+  window.addEventListener("keydown", onEscKeyPress);
+  instance = basicLightbox.create(
+    `<img src="${event.target.parentNode.href}">`
+  );
+  instance.show();
+}
+
+function onEscKeyPress(event) {
+  if (event.code === "Escape") {
+    instance.close();
+    window.removeEventListener("keydown", onEscKeyPress);
+  }
+}
